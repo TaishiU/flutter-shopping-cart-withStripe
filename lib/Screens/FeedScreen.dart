5b6684774e71e8b0da79c743295017489fa2a6dc
TaishiU <nbadreamteam3@gmail.com>
2021-10-12 12:15:48 +0900

    プッシュ通知(バックグラウンド)

--- lib/Screens/FeedScreen.dart
+++ lib/Screens/FeedScreen.dart
@@ -1,7 +1,10 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:shopping_cart/Screens/Chat/MessageScreen.dart';
 import 'package:shopping_cart/Screens/HomeScreen.dart';
+import 'package:shopping_cart/main.dart';
 
 class FeedScreen extends StatefulWidget {
   final String currentUserId;
@@ -16,6 +19,40 @@ class FeedScreen extends StatefulWidget {
 
 class _FeedScreenState extends State<FeedScreen> {
   int _selectedTab = 0;
+  final FirebaseMessaging _firebaseMessaging = FirebaseMessaging.instance;
+
+  _getToken() {
+    _firebaseMessaging.getToken().then((deviceToken) {
+      print('device token: $deviceToken');
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _getToken();
+
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      print("フォアグラウンドでメッセージを受け取りました");
+      RemoteNotification? notification = message.notification;
+      AndroidNotification? android = message.notification!.android;
+
+      if (notification != null && android != null) {
+        flutterLocalNotificationsPlugin.show(
+            notification.hashCode,
+            notification.title,
+            notification.body,
+            NotificationDetails(
+              android: AndroidNotificationDetails(
+                channel.id,
+                channel.name,
+                channelDescription: channel.description,
+                icon: 'launch_background',
+              ),
+            ));
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,19 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:shopping_cart/Screens/FeedScreen.dart';
 import 'package:shopping_cart/Screens/Login/LoginScreen.dart';
 
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+/*バックグラウンド用*/
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
   await Firebase.initializeApp();
-  await dotenv.load(fileName: ".env");
-  runApp(MyApp());
+  print('バックグラウンドでメッセージを受け取りました！');
 }
 
+/*フォアグラウンド用*/
 final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
     FlutterLocalNotificationsPlugin();
 
@@ -23,6 +24,17 @@ const AndroidNotificationChannel channel = AndroidNotificationChannel(
       'This channel is used for important notifications.', // description
   importance: Importance.high,
 );
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  await dotenv.load(fileName: ".env");
+  /*バックグラウンド用*/
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+  runApp(MyApp());
+
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
